58ef0263277777f23dffdad286e7a47a3eddaafb
Aaron Brown <aarbrown109@gmail.com>
2023-07-30 20:13:04 -0400

    adds image support to emails and turnstile validation

--- app/api/club/route.js
+++ app/api/club/route.js
@@ -1,19 +1,42 @@
-import mime from "mime";
+/*import mime from "mime";
 import { join } from "path";
 import { stat, mkdir, writeFile } from "fs/promises";
-import * as dateFn from "date-fns";
+import * as dateFn from "date-fns";*/
 import { NextResponse, NextRequest } from 'next/server';
 import db from '../../lib/db';
 import { Club, Game } from '../../models/clubs';
+import { transporter } from '../../lib/email';
+
+const verifyEndpoint = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+const secret = process.env.TURNSTILE_SECRET_KEY;
 
 export async function POST(request) {
   const formData = await request.formData();
-  console.log(formData);
+
+  const { token } = formData;
+  // before parsing the form data, validate the turnstile token
+  const res = await fetch(verifyEndpoint, {
+    method: 'POST',
+    body: `secret=${encodeURIComponent(secret)}&response=${encodeURIComponent(token)}`,
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded'
+    }
+  })
+
+  const data = await res.json()
+  if (!data.success) {
+    return NextResponse.json(
+      { error: "Invalid token", success: false },
+      { status: 400 }
+    );
+  }
 
   const file = formData.get("file");
   let filePath = null;
+  let buffer;
   if (file) {
-    const buffer = Buffer.from(await file.arrayBuffer());
+    buffer = Buffer.from(await file.arrayBuffer());
+    /*
     const relativeUploadDir = `/uploads/${dateFn.format(Date.now(), "dd-MM-Y")}`;
     const uploadDir = join(process.cwd(), "public", relativeUploadDir);
 
@@ -48,7 +71,7 @@ export async function POST(request) {
         { error: "Something went wrong." },
         { status: 500 }
       );
-    }
+    }*/
   }
 
   const clubData = {
@@ -66,8 +89,26 @@ export async function POST(request) {
   }
 
   try {
+    // create club in database
     await db();
     const club = await Club.create(clubData)
+
+    // send email to admin account notifying us about the new club
+    const mailOptions = {
+      to: 'fgcfinder83@gmail.com',
+      subject: 'Test',
+      text: 'Test email'
+    };
+
+    if (buffer) {
+      // add image to email as an attachment if one was provided
+      mailOptions.attachments = [
+        {filename: file.name, content: buffer}
+      ]
+    }
+
+    await transporter.sendMail(mailOptions);
+
     return NextResponse.json(
       { success: true },
       { status: 200 }
